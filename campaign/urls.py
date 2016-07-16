2706a3efdf7c296ad56b6479ffeec7f6fbb5d654
toast38coza <info@38.co.za>
2016-07-16 16:20:16 +0200

    link up the more link to the campaign detail page

--- campaign/urls.py
+++ campaign/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import CampaignList, CampaignDetail
 
 urlpatterns = [
-    url(r'^$', CampaignList.as_view()),
-    url(r'^(?P<slug>[\w-]+)/$', CampaignDetail.as_view()),
+    url(r'^$', CampaignList.as_view(), name='campaign_list'),
+    url(r'^(?P<slug>[\w-]+)/$', CampaignDetail.as_view(), name='campaign_detail'),
 ]
