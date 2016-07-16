ad1789778fe3427730b9bc1cc332391bcb6b943c
toast38coza <info@38.co.za>
2016-07-16 11:19:44 +0200

    delete env

--- py3.4/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/toast38coza/Projects/Toast38coza/FlashGiving/py3.4/bin/python3.4
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
