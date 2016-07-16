05bbe2fb17165d64ceec7d566315ea006e215428
toast38coza <info@38.co.za>
2016-07-16 13:49:19 +0200

    Adds test coverage

--- campaign/tests.py
+++ campaign/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class HomePageTestCase(TestCase):
+
+	def setUp(self):
+		self.c = Client()
+
+	def test_get_is_200(self):
+
+		response = self.c.get("/")
+		assert response.status_code == 200
+
+class CampaignPageTestCase(TestCase):
+
+	def setUp(self):
+		self.c = Client()
+
+
+	def test_get_is_200(self):
+
+		response = self.c.get("/nonexistant-campaign/")
+		assert response.status_code == 404, \
+			'Expect 404. Got: {}' . format (response.status_code)
