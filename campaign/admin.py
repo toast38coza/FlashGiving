ba0fb028f30ea17dbaaf8e3893d58ce5ab033b96
Ian Roberts <ian@tangentsolutions.co.za>
2016-07-16 13:15:42 +0200

    Added basics of the admin site

--- campaign/admin.py
+++ campaign/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class CharityAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description', 'website')
+
+class CampaignAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description', 'raised')
+
+class TeamAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description')
+
+admin.site.register(Charity, CharityAdmin)
+admin.site.register(Campaign, CampaignAdmin)
+admin.site.register(Team, TeamAdmin)
--- campaign/views.py
+++ campaign/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Campaign 
-
+from .models import Campaign
 
 class CampaignList(ListView):
     model = Campaign
